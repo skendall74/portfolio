6bbff97c6e0ddad7314ea6caa87866cc3cd24206
skendall <shannonckendall@gmail.com>
2019-02-01 20:17:33 -0500

    Mail Fix

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,16 +1,18 @@
    (function () {
        emailjs.init("user_ix2c9dvrdsdZevSF2btCc");
    })();
+    
 
-   window.onload = function () {
-       document.getElementById('contactSubmit').addEventListener('submit', function (event) {
-           event.preventDefault();
-           emailjs.send("gmail", "template_UyMXqtgl");
-           console.log('message sent');
-           alert("Your email has been sent, thank you for reaching out!\nI'll be back to you as soon as possible.\n -Shannon");
+     window.onload = function () {
+         document.getElementById('contact-form').addEventListener('submit', function (event) {
+             event.preventDefault();
+            //  this.contact_number.value = Math.random() * 100000 | 0;
+             emailjs.sendForm('gmail', 'template_UyMXqtgl', this);
+             console.log('message sent');
+             alert("Your email has been sent, thank you for reaching out!\nI'll be back to you as soon as possible.\n -Shannon");
+         });
+     };
 
-       });
-   };
 
 // // prevent page refresh with contact form
 // $("#contactSubmit").on("click", e => {
