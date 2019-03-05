f3b1d9451e76655b5fe6e2233a62462fcefa7dc9
skendall <shannonckendall@gmail.com>
2019-03-05 11:18:46 -0500

    More mail

--- assets/js/email.js
+++ assets/js/email.js
@@ -9,7 +9,7 @@ var params = {
 window.onload = function () {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
-        emailjs.sendForm('gmail', 'template_UyMXqtgl', params);
+        emailjs.sendForm('gmail', 'template_UyMXqtgl', this);
         console.log('message sent');
 
         alert("Your email has been sent, thank you for reaching out!\nI'll be back to you as soon as possible.\n -Shannon");
