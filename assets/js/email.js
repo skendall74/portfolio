2232cd8c80f36c140b5916cd052dd058452a253b
Shannon <shannonckendall@gmail.com>
2019-05-16 08:27:54 -0400

    Mail Fix for Mail.JS Bug

--- assets/js/email.js
+++ assets/js/email.js
@@ -7,7 +7,7 @@ var params = {
 }
 
 window.onload = function () {
-    document.getElementById('contact-form').addEventListener('contactSubmit', function (event) {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
         emailjs.sendForm('gmail', 'template_UyMXqtgl', this);
         console.log('message sent');
